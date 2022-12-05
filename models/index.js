6fb2c15410c584f508d2474c2b970d8361570fbd
keshavdhakane <68534014+keshavdhakane@users.noreply.github.com>
2022-12-05 10:38:50 +0530

    Add files via upload

---
+++ models/index.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+mongoose.Promise = global.Promise;
+const db = {};
+db.mongoose = mongoose;
+db.user = require("./user.model");
+db.role = require("./role.model");
+db.UserTask = require("./usertask.model");
+db.UserAssignTask = require("./UserAssignTask.model");
+db.ROLES = ["user", "admin", "moderator"];
+module.exports = db;
