6c38041b593469d28d0cf6594dec4c96810ff976
keshavdhakane <68534014+keshavdhakane@users.noreply.github.com>
2022-12-05 10:50:15 +0530

    Delete config directory

--- config/db.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  HOST: "localhost",
-  PORT: 27017,
- DB: "mongodb://localhost:27017/typing"
-//  DB: "mongodb+srv://keshav:YgLzdLuOoMO6L4rw@cluster0.ouus9.mongodb.net/typIngDb"   ///TEST  
-};
