d8d629d6010c50f45f8e4f0160dcd216c7efd0f9
keshavdhakane <68534014+keshavdhakane@users.noreply.github.com>
2022-12-05 10:51:08 +0530

    Update db.config.js

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -2,8 +2,6 @@ module.exports = {
   HOST: "localhost",
   PORT: 27017,
  DB: "mongodb://localhost:27017/typing"
- //DB: "mongodb+srv://keshav:YgLzdLuOoMO6L4rw@cluster0.ouus9.mongodb.net/typIngDb"   ///TEST
- // DB: "mongodb+srv://moneylab:Z7E5gOsE808eG6AK@cluster0.e61hx.mongodb.net/typingResume"   ///LIVE
- // DB: "mongodb+srv://emplofit:P9oQKv1G54EgIp5e@cluster0.gdn6jsj.mongodb.net/typingResume"   ///LIVE
+ 
   
 };
